f40fb2dd40f81ea7876af0905d72c6878bee35ee
seventrio <dimakrikunov12@gmail.com>
2020-12-17 23:18:23 +0200

    fix redux-devtools bug

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './redux/reducers';
 
 const store = createStore(
     rootReducer,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools(
+        applyMiddleware(thunk)
     )
 );
 
