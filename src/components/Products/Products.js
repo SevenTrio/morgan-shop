e04ec8fef0efaa1d22d1f06c41f2690eb352dcd6
seventrio <dimakrikunov12@gmail.com>
2020-12-22 19:19:29 +0200

    add products autoscroll

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useBreakpoints } from "../../customHooks/useBreakpoints";
+import { useStateWithCallback } from "../../customHooks/useStateWithCallback";
 import { useSelector } from "react-redux";
 import { selectProductsByCategoryWithSorting } from "../../redux/selectors/productsSelectors";
 import ProductCard from "../ProductCard/ProductCard";
@@ -7,16 +9,31 @@ import Button from "../Button/Button";
 import './Products.scss';
 
 const Products = ({ category, className = "" }) => {
+    const { breakMdDown, breakSmDown } = useBreakpoints();
     const sortType = useSelector((state) => state.products.sortType);
     const products = useSelector(selectProductsByCategoryWithSorting(category, sortType));
-    const [productsPresented, setProductsPresented] = useState(12);
+    const [productsPresented, setProductsPresented] = useStateWithCallback(12);
+
+    const handleScroll = () => {
+        let scrollIndent = 24 + 43 + 16;                    /* button mt + button h + page pb */
+        if (breakMdDown) scrollIndent = 65 + 24 + 43 + 16;  /* header h + button mt + button h + page pb */
+        if (breakSmDown) scrollIndent = 48 + 12 + 38 + 6;   /* header h + button mt + button h + page pb */
+
+        window.scrollBy({
+            top: document.documentElement.clientHeight - scrollIndent,
+            behavior: 'smooth'
+        })
+    }
 
     useEffect(() => {
         setProductsPresented(12);
     }, [category, sortType]);
 
     const showMoreProducts = () => {
-        setProductsPresented(prevState => prevState + 12);
+        setProductsPresented(
+            prevState => prevState + 12,
+            handleScroll
+        )
     }
 
     return (
